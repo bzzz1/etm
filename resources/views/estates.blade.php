8ca7eb09d44a2816849fc3287eacadc62d9dca55
Bogdan Kootz <bodyakootz@gmail.com>
2015-06-28 13:43:09 +0300

    footer

--- resources/views/estates.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('/layouts/layout')
-@extends('/partials/header')
-@extends('/partials/navbar')
-@extends('/partials/flash_messages')
-@extends('/partials/footer')
-@extends('/partials/estates')
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,34 +6,19 @@
 					{{ HTML::image("img/layout/logo_footer.png", "logo", ['class'=>'logo']) }}
 				</a>	
 			</div>
-			<div class="footer_descript left">
-				<p>Агентство недвижимости</p>
-				<p>"Крым Вилла" предлагает частные</p>
-				<p>дома, коттеджы и земельные участки для продажи в Крыму.</p>
-			</div>
-			{{-- UNKOMMENT FOR A SIMPLE NAVBAR IN FOOTER --}}
-			<div class="footer_nav_block">
-				<ul class="footer_nav left">
-					<li class="active"><a href="/">поиск объектов</a></li>
-					<li class=""><a href="/articles">новости</a></li>
-					<li class=""><a href="/how_to">как арендовать</a></li>
-				</ul>
-				<ul class="footer_nav right">
-					<li class=""><a href="/rent_sale">сдать/продать</a></li>
-					<li class=""><a href="/selected">избранные</a></li>
-					<li class=""><a href="/contacts">контакты</a></li>
-				</ul>
+			<div class="footer_descript">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nemo rerum odio, ad harum cupiditate accusantium, error eum incidunt laboriosam, asperiores eius. Tempora culpa, quae numquam magnam odit fuga eum sint, possimus aliquam, eaque labore est. Libero sunt nobis maiores. Dolorem dolores at illum cum explicabo laudantium quidem tenetur repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius iure, vero officia mollitia hic necessitatibus praesentium atque, consequatur ullam provident?</p>
 			</div>
 			<div class="copyright">
 				<div class="client_copy">
 					<p>
-						<i class="fa fa-copyright"></i> 2015 Krim Villa&nbsp All rights reserved
+						<i class="fa fa-copyright"></i> 2015 VEC&nbsp All rights reserved
 					</p>
 					{{-- <p></p> --}}
 				</div>
 				<div class="our_link">
 					<p>made by
-						<a href="http://www.dev.bzzz.biz.ua">bzzz! web development studio</a>
+						<a href="http://www.dev.bzzz.biz.ua">[bzzz!]* web development studio</a>
 					</p>
 				</div>
 			</div>	
