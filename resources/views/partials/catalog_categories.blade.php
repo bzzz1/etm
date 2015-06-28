fece66a3d2366cb932bc9c037a84ae9b1c2418d3
Bogdan Kootz <bodyakootz@gmail.com>
2015-06-28 21:17:49 +0300

    news and catalog_categories

---
+++ resources/views/partials/catalog_categories.blade.php
@@ -0,0 +1,155 @@
+<div class="categories_block">
+	<h1>категории товаров</h1>
+	<div class="categories">
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+		<div class="one_category">
+			<div class="category_img">
+				<a href="" class="img_link">
+					{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+				</a>
+				<div class="mask">
+					<a href="">
+						<i class="fa fa-search fa-3x"></i>
+					</a>
+				</div>
+			</div>
+			<div class="title">
+				<a class="category_title">Кабельно-проводниковая продукция</a>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ resources/views/partials/latest_news.blade.php
@@ -0,0 +1,87 @@
+<div class="news_block">
+	<h2>последние новости</h2>
+	<div class="one_new">
+		<div class="new_img">
+			<a href="" class="img_link">
+				{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+			</a>
+			<div class="mask">
+				<a href="">
+					<i class="fa fa-plus fa-5x"></i>
+				</a>
+			</div>
+		</div>
+		<div class="title">
+			<a href="" class="new_title">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости</a>
+			<a href="" class="new_title full">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости </a>
+		</div>
+		<div class="date_block">
+			<p class="date">20 июня 2015</p>
+			<i class="fa fa-calendar fa-lg"></i>
+		</div>
+		<a href="" class="btn more_btn">подробнее</a>
+	</div>
+	<div class="one_new">
+		<div class="new_img">
+			<a href="" class="img_link">
+				{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+			</a>
+			<div class="mask">
+				<a href="">
+					<i class="fa fa-plus fa-5x"></i>
+				</a>
+			</div>
+		</div>
+		<div class="title">
+			<a href="" class="new_title">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости</a>
+			<a href="" class="new_title full">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости </a>
+		</div>
+		<div class="date_block">
+			<p class="date">20 июня 2015</p>
+			<i class="fa fa-calendar fa-lg"></i>
+		</div>
+		<a href="" class="btn more_btn">подробнее</a>
+	</div>
+	<div class="one_new">
+		<div class="new_img">
+			<a href="" class="img_link">
+				{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+			</a>
+			<div class="mask">
+				<a href="">
+					<i class="fa fa-plus fa-5x"></i>
+				</a>
+			</div>
+		</div>
+		<div class="title">
+			<a href="" class="new_title">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости</a>
+			<a href="" class="new_title full">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости </a>
+		</div>
+		<div class="date_block">
+			<p class="date">20 июня 2015</p>
+			<i class="fa fa-calendar fa-lg"></i>
+		</div>
+		<a href="" class="btn more_btn">подробнее</a>
+	</div>
+	<div class="one_new">
+		<div class="new_img">
+			<a href="" class="img_link">
+				{{HTML::image('img/layout/fish.png', 'category_name', ['class'=>''])}}
+			</a>
+			<div class="mask">
+				<a href="">
+					<i class="fa fa-plus fa-5x"></i>
+				</a>
+			</div>
+		</div>
+		<div class="title">
+			<a href="" class="new_title">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости</a>
+			<a href="" class="new_title full">Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости Заголовок новости </a>
+		</div>
+		<div class="date_block">
+			<p class="date">20 июня 2015</p>
+			<i class="fa fa-calendar fa-lg"></i>
+		</div>
+		<a href="" class="btn more_btn">подробнее</a>
+	</div>
+</div>
